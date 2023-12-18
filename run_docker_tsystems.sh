7b1cf300885a4dc32e594cf600cf2da37f1f193b
hanan <hanan@driveu.auto>
2023-12-18 12:22:56 +0200

    removed run_docker_tsystems.sh

--- run_docker_tsystems.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -z "$1" ]; then
-  echo "Error: the SIP number is not specified"
-  echo "Usage: run_docker_tsystems.sh <sipNumber"
-  echo "   Example: run_docker_tsystems.sh echoTest"
-  exit -1
-fi
-docker run -it --network host --privileged  --device /dev/snd:/dev/snd  du-sip-client:1.0.0 ./run_client.sh --profile tsystems --sip-number $1@localhost
